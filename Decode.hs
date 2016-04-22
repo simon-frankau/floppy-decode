91b9e97ba70e77576e5d2242b0e91edf63b8f4bf
Simon Frankau <sgf@arbitrary.name>
2016-04-22 20:09:34 +0100

    Horrible basic rewrite to automatically cope with glitches between sections

--- Decode.hs
+++ Decode.hs
@@ -88,42 +88,31 @@ toBitPattern = concatMap (\i -> True :
 -- Interpret the bit stream
 --
 
--- Argh, I think there's a timing issue, possibly caused by
--- overwriting the sector?
-patchTiming :: [Integer] -> [Integer]
-patchTiming (2:2:6:xs) = xs
-patchTiming (x:xs) = x : patchTiming xs
-patchTiming [] = []
-
 -- Convenience...
 prettyBits :: [Bool] -> String
 prettyBits = map (\x -> if x then '1' else '0')
-
--- Sync is 4E
--- That is, 01001110
--- With MFM, this becomes 0010010010101001
---                        . . . . . . . .
-
--- We'll look for the sync byte repeated 5 times, ignoring the fact that
--- it might appear in real data, etc.
-syncPattern :: [Bool]
-syncPattern = concat $ take 5 $ repeat oneSync where
-  oneSync = map (== '1') "0010010010101001"
-
-isSynced :: [Bool] -> Bool
-isSynced xs = take (length syncPattern) xs == syncPattern
-
-getSync :: [Bool] -> [Bool]
-getSync = head . filter isSynced . L.tails
 
 -- Remove the sync bits, un-MFMing the data stream
 unMFM :: [Bool] -> [Bool]
 unMFM (x:_:xs) = x : unMFM xs
 unMFM xs = xs
 
+-- Insert sync bits. Doesn't put a sync bit at the end, as we don't
+-- know what's next.
+addMFM :: [Bool] -> [Bool]
+addMFM [] = []
+addMFM [x] = [x]
+addMFM (False : rest@(False : _)) = False : True : addMFM rest
+addMFM (x : rest) = x : False : addMFM rest
+
 -- Make a bit stream into a number
 numberify :: [Bool] -> Integer
 numberify = L.foldl' (\x y -> x * 2 + fromIntegral (fromEnum y)) 0
+
+-- And a number into a stream
+denumberify :: Integer -> [Bool]
+denumberify = L.reverse . take 8 .
+              L.unfoldr (\x -> Just (x `mod` 2 == 1, x `div` 2))
 
 -- Gap 4A = 80 bytes 0x4E
 -- Sync = 12 bytes 0x00
@@ -182,6 +171,20 @@ expect what (e:es) = do
                          show val ++ " instead of " ++ show e
 expect _ [] = return ()
 
+-- Search for the start of the gap.
+findGap :: DiskM ()
+findGap = do
+  b <- tryRead 0x4E
+  if b
+   then do
+     record $ Note "Found sync"
+     return ()
+   else do
+     record $ Note "Skipping one bit"
+     readBits 1
+     findGap
+
+-- Read through the gap.
 skipGap :: DiskM ()
 skipGap = do
   succeeded <- tryRead 0x4E
@@ -196,35 +199,65 @@ skipGapAndSync = do
 
 skipHeader :: DiskM ()
 skipHeader = do
-  skipGapAndSync
-  expect "IAM" [0xc2, 0xc2, 0xc2, 0xfc]
+  skipGap -- TODO: Sync
+  skipHeader'
+    where
+      skipHeader' = do
+        b <- tryRead 0xc2
+        if b
+         then expect "IAM" [0xc2, 0xc2, 0xfc]
+         else do
+           record $ Note "Finding IAM"
+           readBits 1
+           skipHeader'
 
 readSectorHeader :: DiskM String
 readSectorHeader = do
-  skipGapAndSync
-  expect "IDAM" [0xa1, 0xa1, 0xa1, 0xfe]
-  sectorId <- readInts 4
-  -- Skip CRC
-  readInts 2
-  record $ Note $ "Read sector head for sector " ++ show sectorId
-  return $ show sectorId
+  skipGap -- TODO: Sync
+  readSectorHeader'
+    where
+      readSectorHeader' = do
+        b <- tryRead 0xa1
+        if b
+          then do
+            expect "IDAM" [0xa1, 0xa1, 0xfe]
+            sectorId <- readInts 4
+            -- Skip CRC
+            readInts 2
+            record $ Note $ "Read sector head for sector " ++ show sectorId
+            return $ show sectorId
+        else do
+          record $ Note "Finding IDAM"
+          readBits 1
+          readSectorHeader'
 
 readSectorBody :: DiskM [Integer]
 readSectorBody = do
-  skipGapAndSync
-  expect "DAM" [0xa1, 0xa1, 0xa1]
-  b <- readInt
-  if b /= 0xfb && b /= 0xf8
-   then lift . throwE $ "Expected DAM, got " ++ show b
-   else do
-     res <- readInts 512
-     record $ Note "Read sector body"
-     -- Skip CRC
-     readInts 2
-     return res
+  skipGap -- TODO: Sync
+  readSectorBody'
+    where
+      readSectorBody' = do
+        b <- tryRead 0xa1
+        if b
+          then do
+            expect "DAM" [0xa1, 0xa1]
+            b <- readInt
+            if b /= 0xfb && b /= 0xf8
+             then lift . throwE $ "Expected DAM, got " ++ show b
+             else do
+               res <- readInts 512
+               record $ Note "Read sector body"
+               -- Skip CRC
+               readInts 2
+               return res
+          else do
+            record $ Note "Finding DAM"
+            readBits 1
+            readSectorBody'
 
 readImage :: DiskM [(String, [Integer])]
 readImage = do
+  findGap
   skipHeader
   readImageAux
     where
@@ -252,8 +285,7 @@ main = do
   -- Get stats on the transitions - they should group nicely...
   printBitStats transitions
   -- And print the data
-  let bitStream = getSync $ toBitPattern $
-                  patchTiming $ toBaseBitRate transitions
+  let bitStream = toBitPattern $ toBaseBitRate transitions
   putStrLn $ show $ toBaseBitRate transitions
   putStrLn $ show $ prettyBits bitStream
   putStrLn $ show $ runWriter $ runExceptT $ runStateT readImage bitStream
